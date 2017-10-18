cdd07ce28eff30cf149892d43a0adc9a3ee992e1
Timo van Milligen <cobravspanda01@gmail.com>
2017-10-18 17:28:57 +0200

    simulatiebegin, excelwriter begin

---
+++ project/ExcelWriter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace project
+{
+    class ExcelWriter
+    {
+        string filepath;
+        public ExcelWriter(string filepath)
+        {
+            this.filepath = filepath;
+        }
+        public void WriteStuff()
+        {
+            Application oXL;
+            _Workbook oWB;
+            _Worksheet oSheet;
+            Range oRng;
+            object misvalue = System.Reflection.Missing.Value;
+
+            // try
+            /// {
+            //Start Excel and get Application object.
+            oXL = new Application();
+            oXL.Visible = true;
+
+            //Get a new workbook.
+            oWB = (_Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (_Worksheet)oWB.ActiveSheet;
+
+            //Add table headers going cell by cell.
+            oSheet.Cells[1, 1] = "Sudoku Size";
+            oSheet.Cells[1, 2] = "Runtime (milliseconds)";
+            oSheet.Cells[1, 3] = "S value";
+            oSheet.Cells[1, 4] = "Random Walk Threshhold";
+            //Format A1:D1 as bold, vertical alignment = center.
+            oSheet.get_Range("A1", "D1").Font.Bold = true;
+            oSheet.get_Range("A1", "D1").VerticalAlignment =
+                 XlVAlign.xlVAlignCenter;
+
+           
+            // oSheet.Unprotect();
+
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            string name = "kek";
+            oWB.SaveAs(filepath + name, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            oWB.Close();
+            //  }
+            //  catch
+            // {
+            //     Console.WriteLine("error writing to excel file");
+            // }
+        }
+    }
+}
--- project/Program.cs
+++ project/Program.cs
@@ -6,64 +6,137 @@ using System.Threading.Tasks;
 
 namespace project
 {
-    public class Program
+   static class Program
     {
-        int currentTimeFrame; //current time
-        int maxTimeFrame; //closing time
-        int peakTime; //Time with peak amount of incoming visitors;
-        int peakNewVisitors; //peak amount of incoming visitors;
-        int currentVisitors; //amount of current visitors
-        int visitorsInQueue;
-        int numberOfFoodtrucks;
+        const string excelFilePath = "C:\\Users\\Timo\\Documents\\onderzoeksmethoden\\";
+        static double value = 0.2;
+        static int currentTimeFrame; //current time
+        static int maxTimeFrame; //closing time
+        static int peakTime; //Time with peak amount of incoming visitors;
+        static int peakNewVisitors; //peak amount of incoming visitors;
+        static int visitorsInQueue; //amount of overall visitors waiting in line
+        static int numberOfFoodtrucks;
+        static Dictionary<int, FoodTruck> layout; //Foodtrucks and their numbered location in the layout;
+        static List<Visitor> visitors;
+        static ExcelWriter writer;
+        static Random r; //random number generator
 
-
-        double value = 0.2;
-        int incomingVisitors() //incoming visitors on current timeframe
+        static int incomingVisitors() //incoming visitors on current timeframe
         {
             int deltaPeakTime = Math.Abs(currentTimeFrame - peakTime); //how far away from peaktime it is
             return (int)(peakNewVisitors / (1 + deltaPeakTime * value)); //incoming visitors
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Initialise();
+            //TODO: Initialise foodtrucks
+            while (currentTimeFrame<maxTimeFrame)
+            {
+                for (int i = 0; i < incomingVisitors(); i++)
+                {
+                    visitors.Add(new Visitor(currentTimeFrame));
+                }
+                foreach(Visitor v in visitors)
+                {
+                    if (!v.waiting)
+                    {
+                        //Chance of leaving
+                        if (r.NextDouble() <= v.chanceOfLeaving())
+                        {
+                            visitors.Remove(v);                     //Leave
+                        }
+
+                        //if at a foodtruck
+                        if (layout.ContainsKey(v.location))
+                        {
+                            FoodTruck truck = layout[v.location]; //Get the current food truck
+                            double rndValue = r.NextDouble();
+                            if (rndValue <= v.chanceQueueing(truck)) //get in line at the food truck
+                                v.GetInLine(truck);
+                            else                                     //move somewhere else
+                            {
+                                double rnd = r.NextDouble();
+
+                            }
+                        }
+                    }
+
+                }
+
+
+                currentTimeFrame++;
+            }
+
+
+        }
+        static void Initialise()
+        {
+            writer = new ExcelWriter(excelFilePath);
+            currentTimeFrame = 0;
+            maxTimeFrame = 54;
+            peakTime = 30;
+            peakNewVisitors = 50;
+            currentVisitors = 0;
+            visitorsInQueue = 0;
+            numberOfFoodtrucks = 10;
+            layout = new Dictionary<int, FoodTruck>();
+            visitors = new List<Visitor>();
+            writer = new ExcelWriter(excelFilePath);
+            r = new Random();
+
         }
     }
     class FoodTruck
     {
         public double appeal;
-        public int queue;
+        public Queue<Visitor> queue;
         int serviceRate; //amount of customers helped in 1 timeframe
         int location;
-
+        int[] queueArray; //array to store the queue length on each timeframe
+        public Dictionary<FoodTruck, int> neighbours; //neighbours with the distance to the neighbour
         public double queueTime()
         {
-            return (queue / serviceRate);
+            return (queue.Count / serviceRate);
         }
 
-        public FoodTruck()
+        public FoodTruck(int timeframes)
         {
-
+            queueArray = new int[timeframes];
+        }
+        public void AddNeighbour(FoodTruck truck, int distance)
+        {
+            neighbours.Add(truck, distance);
         }
 
     }
     class Visitor
     {
-        bool waiting;
-        int location;
+        public bool waiting;
+        public int location;
         int arrivedTime; // arrived time
         int visitedFoodtrucks; // amount of foodtrucks visited
         double maxQueueTime = 3; //maximum acceptable queuetime for a visitor
+        public Dictionary<int, bool> locationsVisited; //location and wether or not he has visited it
 
-        public Visitor()
+        public Visitor(int arrivedTime)
         {
-
+            this.arrivedTime = arrivedTime;
+            location = 0;
+            visitedFoodtrucks = 0;
+            waiting = false;
+            locationsVisited = new Dictionary<int, bool>();
         }
-        double chanceOfLeaving()
+        public double chanceOfLeaving()
         {
             double res = 1;
             return res;
         }
-
+        public void GetInLine(FoodTruck truck)
+        {
+            truck.queue.Enqueue(this);
+            waiting = true;
+            locationsVisited[location] = true;
+        }
         public double chanceQueueing(FoodTruck truck)
         {
             return Math.Max(0, (truck.appeal - truck.appeal * (truck.queueTime() / maxQueueTime)));
